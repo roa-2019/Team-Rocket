6e8aec8572d321e5963798a5017c3c0126206e4d
rosedhlakama <rosejembe@live.com>
2019-09-25 18:50:47 +1200

    rocket page layout complete

--- client/components/Rocket.jsx
+++ client/components/Rocket.jsx
@@ -1,17 +1,25 @@
 import React, { Component, Fragment } from 'react'
 
 class Rocket extends Component {
-    render(){
-        return(
-            <Fragment>
-            <div className='rocket'>
+  render() {
+    return (
+      <Fragment>
+        <div className='rocket'>
+          <div className="details-panel col b">
+            <p>left panel</p>
+          </div>
+          <div className="rocket-panel col b">
+      <div className="rocket-wrapper">
+        <div className="rocket-nose third b"></div>
+        <div className="rocket-body third b"></div>
+        <div className="rocket-fins third b"></div>
+      </div>
+          </div>
 
-            {/* start coding here */}
-
-            </div>
-            </Fragment>
-        )
-    }
+        </div>
+      </Fragment>
+    )
+  }
 }
 
 export default Rocket
