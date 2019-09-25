87a78a130c148e10ee4fff386a5de16e1cf7d15b
Alex-Abernethy <alexander.abernethy@gmail.com>
2019-09-26 09:30:13 +1200

    updated simpler reducer

--- client/reducers/rocketreducer.js
+++ client/reducers/rocketreducer.js
@@ -1,16 +1,17 @@
 import { NOSE_SHAPE, NOSE_COLOR } from '../actions'
 
 const initialRocketState = {
-  rocket: { noseShape: '', noseColor: '' }
+   noseShape: '',
+   noseColor: ''
 }
 
 const rocketReducer = (state = initialRocketState, action) => {
   switch (action.type) {
     case NOSE_SHAPE:
-      return Object.assign({}, store, { rocket: Object.assign({}, store.rocket, action) });
+      return { ...state, noseShape: action.shape }
 
     case NOSE_COLOR:
-      return Object.assign({}, store, { rocket: Object.assign({}, store.rocket, action) });
+        return { ...state, noseShape: action.color }
 
     default:
       return state
@@ -18,11 +19,3 @@ const rocketReducer = (state = initialRocketState, action) => {
 }
 
 export default rocketReducer
-
-// let store = {
-//   rocket: { noseColor: '', noseShape: '' }
-// }
-
-// let action = { noseColor: 'red' }
-
-// let newStore = Object.assign({}, store, { rocket: Object.assign({}, store.rocket, action) });
