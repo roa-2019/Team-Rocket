d50ed95a6a1bc0a077caae5538f16080fe3ba595
Will Houghton <48467321+WillHoughton@users.noreply.github.com>
2019-09-27 09:54:09 +1200

    new rocket parts

---
+++ client/components/rocket-parts/Nose.jsx
@@ -0,0 +1,12 @@
+import React, { Component, Fragment } from 'react'
+
+class Nose extends Component {
+    return(
+        <!-- Nose -->
+        <ellipse cx="290" cy="210" rx="50" ry='100' stroke="black" stroke-width="1" fill="grey" />
+    )
+
+}
+
+
+export default Nose
---
+++ client/components/rocket-parts/Thruster.jsx
@@ -0,0 +1,13 @@
+import React, { Component, Fragment } from 'react'
+
+class Thruster extends Component {
+    return(
+        <!-- Thruster 1 box (clockwise) -->
+        <polygon points='330 500 330 520 250 520 250 500' fill='gray' stroke='black' stroke-width='1' />
+        
+    )
+
+}
+
+
+export default Thruster
