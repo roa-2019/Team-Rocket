3a1ecd57f557a527cd622b2119585601086c024e
Alex-Abernethy <alexander.abernethy@gmail.com>
2019-09-28 10:34:24 +1200

    changeColor bug fix

--- client/components/Rockets.jsx
+++ client/components/Rockets.jsx
@@ -5,6 +5,7 @@ import { connect } from 'react-redux'
 class Rockets extends Component {
 
   render() {
+    
     return (
       <Fragment>
         <div className='rocket rocket-panel col'>
@@ -16,21 +17,21 @@ class Rockets extends Component {
               {/* Wings */}
 
               {/* Right wing 2 rectangle slightly off */}
-              <polygon points="340 440 390 460 380 550 340 550" fill='grey' stroke='black' stroke-width='1' />
+              <polygon points="340 440 390 460 380 550 340 550" fill='grey' stroke='black' strokeWidth='1' />
               {/* Left wing 2 rectangle slightly off  */}
-              <polygon points="240 440 190 460 200 550 240 550" fill='grey' stroke='black' stroke-width='1' />
+              <polygon points="240 440 190 460 200 550 240 550" fill='grey' stroke='black' strokeWidth='1' />
 
               {/* Rocket nose 1  */}
-              {this.props.noseShape == 'Circle' && <ellipse cx="290" cy="210" rx="50" ry='100' stroke="black" stroke-width="1" fill={this.props.changeColor} />}
-
+              {this.props.noseShape == 'Circle' && <ellipse cx="290" cy="210" rx="50" ry='100' stroke="black" strokeWidth="1" fill={this.props.changeColor} />}
+              {this.props.noseShape == 'Triangle' && <polygon points="240 200 290 120 340 200" stroke='black' stroke-width='1' fill={this.props.changeColor}/>}
               {/* Rocket body 1  */}
-              <polygon points="240 200 340 200 340 500 240 500" fill='grey' stroke='black' stroke-width='1' />
-              <circle cx='290' cy='300' r='20' fill='aqua' stroke='black' stroke-width='4' stroke-opacity='.4' />
-              <circle cx='290' cy='370' r='20' fill='aqua' stroke='black' stroke-width='4' stroke-opacity='.4' />
-              <circle cx='290' cy='440' r='20' fill='aqua' stroke='black' stroke-width='4' stroke-opacity='.4' />
+              <polygon points="240 200 340 200 340 500 240 500" fill='grey' stroke='black' strokeWidth='1' />
+              <circle cx='290' cy='300' r='20' fill='aqua' stroke='black' strokeWidth='4' strokeOpacity='.4' />
+              <circle cx='290' cy='370' r='20' fill='aqua' stroke='black' strokeWidth='4' strokeOpacity='.4' />
+              <circle cx='290' cy='440' r='20' fill='aqua' stroke='black' strokeWidth='4' strokeOpacity='.4' />
 
               {/*Rocket thruster 1 */}
-              <polygon points='320 500 300 540 280 540 260 500' fill='grey' stroke='black' stroke-width='1' />
+              <polygon points='320 500 300 540 280 540 260 500' fill='grey' stroke='black' strokeWidth='1' />
             </svg>
           </div>
         </div>
@@ -42,12 +43,12 @@ class Rockets extends Component {
 function mapStateToProps(state) {
 
   return {
-    noseShape: state.noseShape,
-    changeColor: state.changeColor,
-    bodyShape: state.bodyShape,
-    wingShape: state.wingShape,
-    windowShape: state.windowShape,
-    thrusterShape: state.thrusterShape
+    noseShape: state.rocket.noseShape,
+    changeColor: state.rocket.changeColor,
+    bodyShape: state.rocket.bodyShape,
+    wingShape: state.rocket.wingShape,
+    windowShape: state.rocket.windowShape,
+    thrusterShape: state.rocket.thrusterShape
   }
 }
 
