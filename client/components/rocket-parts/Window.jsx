61421080c8d74ed7f076ec8ecdd5560bfa831979
Will Houghton <48467321+WillHoughton@users.noreply.github.com>
2019-09-27 12:24:09 +1200

    all rocket parts done

--- client/components/rocket-parts/Window.jsx
+++ client/components/rocket-parts/Window.jsx
@@ -16,6 +16,20 @@ class Window extends Component {
                 <polygon points='300 380 300 400 280 400 280 380' fill='aqua' stroke='black' stroke-width='1' />
                 <polygon points='300 400 300 420 280 420 280 400' fill='aqua' stroke='black' stroke-width='1' />
                 <polygon points='300 420 300 440 280 440 280 420' fill='aqua' stroke='black' stroke-width='1' />
+
+                {/* Window set 2 three large circle windows with decent border */}
+                <circle cx='290' cy='300' r='20' fill='aqua' stroke='black' stroke-width='4' fill='grey' stroke-opacity='.4' />
+                <circle cx='290' cy='370' r='20' fill='aqua' stroke='black' stroke-width='4' stroke-opacity='.4' />
+                <circle cx='290' cy='440' r='20' fill='aqua' stroke='black' stroke-width='4' stroke-opacity='.4' />
+
+                {/* Window set 3 three medium size rectangle windows with decent border */}
+                <polygon points="270 290 310 290 310 310 270 310" fill='aqua' stroke='black' stroke-width='4' stroke-opacity='.4' />
+                <polygon points="270 340 310 340 310 360 270 360" fill='aqua' stroke='black' stroke-width='4' stroke-opacity='.4' />
+                <polygon points="270 390 310 390 310 410 270 410" fill='aqua' stroke='black' stroke-width='4' stroke-opacity='.4' />
+
+                {/* Window set 4 two large y axis rectangle panels no fill color only border */}
+                <polygon points="270 410 310 410 310 340 270 340" fill='grey' stroke='black' stroke-width='1' fill-opacity='.0' />
+                <polygon points="270 420 310 420 310 490 270 490" fill='grey' stroke='black' stroke-width='1' fill-opacity='.0' />
             </div>
 
         )
