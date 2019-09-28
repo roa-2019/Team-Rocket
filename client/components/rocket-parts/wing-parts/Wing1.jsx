3af442963978fa2a3969126c345e13a2fd74177a
rosedhlakama <rosejembe@live.com>
2019-09-28 21:48:10 +1200

    initial commit

--- client/components/rocket-parts/wing-parts/Wing1.jsx
+++ client/components/rocket-parts/wing-parts/Wing1.jsx
@@ -1,13 +1,15 @@
 import React, { Component, Fragment } from 'react'
 
+import { connect } from 'react-redux'
+
 class Wing1 extends Component {
     render() {
         return (
             <Fragment>
                 {/* Right Wing 1 paralelogram (clockwise)--> */}
-                <polygon points="340 270 430 440 430 580 340 410" fill='grey' stroke='black' strokeWidth='1' />
+                <polygon points="340 270 430 440 430 580 340 410" fill={this.props.changeColor} stroke={this.props.strokeColor} strokeWidth='1' />
                 {/*<!-- Left Wing 1 paralelogram (mirror right wing) --> */}
-                <polygon points="240 270 150 440 150 580 240 410" fill='grey' stroke='black' strokeWidth='1' ry='1' />
+                <polygon points="240 270 150 440 150 580 240 410" fill={this.props.changeColor} stroke={this.props.strokeColor} strokeWidth='1' ry='1' />
             </Fragment>
 
         )
@@ -15,4 +17,13 @@ class Wing1 extends Component {
 }
 
 
-export default Wing1
+
+function mapStateToProps(state){
+    return{
+        changeColor: state.rocket.changeColor,
+        strokeColor: state.rockect.strokeColor
+    }
+    
+}
+
+export default connect(mapStateToProps)(Wing1)
