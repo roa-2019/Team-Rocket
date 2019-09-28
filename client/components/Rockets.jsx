77c59e2c5f398bc991c3533ef4872c076099f033
rosedhlakama <rosejembe@live.com>
2019-09-28 15:34:34 +1200

    updated labels for radio buttons

--- client/components/Rockets.jsx
+++ client/components/Rockets.jsx
@@ -4,6 +4,8 @@ import { connect } from 'react-redux'
 import Nose1 from './rocket-parts/nose-parts/Nose1';
 import Nose2 from './rocket-parts/nose-parts/Nose2';
 import Nose3 from './rocket-parts/nose-parts/Nose3';
+import Nose4 from './rocket-parts/nose-parts/Nose4';
+import Nose5 from './rocket-parts/nose-parts/Nose5';
 
 import Body1 from './rocket-parts/body-parts/Body1';
 import Body2 from './rocket-parts/body-parts/Body2';
@@ -45,15 +47,16 @@ class Rockets extends Component {
               {this.props.wingShape == 'Large' && <Wing5 />}
             
               {/* Rocket nose 1  */}
-              {this.props.noseShape == 'Circle' && <Nose1 />}
-              {this.props.noseShape == 'Triangle' && <Nose2 />}
-              {this.props.noseShape == 'LongTriangle' && <Nose3 />}
+              {this.props.noseShape == 'Ellipse' && <Nose1 />}
+              {this.props.noseShape == 'Pyramid' && <Nose2 />}
+              {this.props.noseShape == 'Triangular' && <Nose3 />}
+              {this.props.noseShape == 'Top Window' && <Nose4 />}
               
               {/* Rocket body 1  */}
               {this.props.bodyShape == 'Rectangle' && <Body1 />}
-              {this.props.bodyShape == 'Trapazoid' && <Body2 />}
-              {this.props.bodyShape == 'Pillar' && <Body3 />}
-              {this.props.bodyShape == 'Body4' && <Body4 />}
+              {this.props.bodyShape == 'Trapezoid' && <Body2 />}
+              {this.props.bodyShape == 'Hexagon' && <Body3 />}
+              {this.props.bodyShape == 'Tower' && <Body4 />}
 
               {/*Rocket thruster 1 */}
               {this.props.thrusterShape == 'Thruster1' && <Thruster1 />}
--- client/components/rocket-parts/body-parts/Body2.jsx
+++ client/components/rocket-parts/body-parts/Body2.jsx
@@ -4,7 +4,7 @@ class Body2 extends Component {
     render() {
         return (
             <Fragment>
-                {/* Body 2 trapazoid */}
+                {/* Body 2 trapezoid */}
                 <polygon points="200 200 380 200 360 350 220 350 " fill='grey' stroke='black' strokeWidth='1' />
                 <polygon points="240 500 340 500 360 350 220 350" fill='grey' stroke='black' strokeWidth='1' />
             </Fragment>
