585d71698420dacc7adf953ec7592d79902a6df0
john blackwood <AvaBlackwood@johns-MBP.localdomain>
2019-09-30 17:53:47 +1300

    landing page image and logo are working and in correct place. Storyline is appearing but in the wrong place

--- client/components/LandingPage.jsx
+++ client/components/LandingPage.jsx
@@ -8,16 +8,21 @@ class LandingPage extends Component {
         return (
             <Fragment>
                 <div>
-                    
-                    <div className='Landingimg'>
-                        <img src="/images/Landingimg.jpg" onClick={() => {
-                            this.props.dispatch(GoToBuildRocket())
-                        }
-                        }>
- 
-                        </img>
+                    <div className='landingLogo'>
+                    <img src="/images/landinglogoplanet.png" width="1200px" className="landinglogo" /> 
                     </div>
-                </div>
+                    <div className='Landingimg'>
+                      
+                        <div className ="landingImg" onClick={() => {
+                            this.props.dispatch(GoToBuildRocket())
+                        }}></div>
+                           <div class="typewriter">
+                        <h1>On an assigned mission from NZ Space Agency to explore mars and gather data on the existence of habitable conditions and the possibility of presence of life. Your Rocket Ship was entangled in a meteor shower and streams of cosmic debris. You have landed safely on Mars, however there is severe damage to the exterior panelling of your rocket as well as engine damage. The rocket is still un-operable and needs to be re-built. Your mission is to rebuild your rocket and return safely to Earth. Do you accept this mission?</h1>
+                        </div>
+                         </div>  
+                    </div>
+
+                 
             </Fragment>
         )
  
