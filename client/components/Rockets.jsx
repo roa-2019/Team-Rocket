16f2d9ef8a6420058805f7f7d3fdf0e8506caffc
mariawenrose <42329353+mariawenrose@users.noreply.github.com>
2019-10-01 10:17:53 +1300

    changed order or robot errors

--- client/components/Rockets.jsx
+++ client/components/Rockets.jsx
@@ -55,10 +55,10 @@ class Rockets extends Component {
           </div>
 
           <div className = 'robot__error'>
-            {this.props.wingShape == "SmallTriangle" && <p className="robot__error--wing typewriter b">"The Rocket will not take off with wings that small"</p>}
-            {this.props.noseShape == "Pyramid" && <p className="robot__error--nose">"Oops you'll have to choose a nose that isn't wonky"</p>}
-            {this.props.thrusterShape == 'Thruster1' && <p className="robot__error--thruster">"Oh no, that's way too small - your're going to have to choose another one"</p>}
             {this.props.bodyShape == "Rectangle" && <p className="robot__error--bodyShape">"Do you see me? How am I supposed to fit into such a thin Rocket?!"</p>}
+            {this.props.noseShape == "Pyramid" && <p className="robot__error--nose">"Oops you'll have to choose a nose that isn't wonky"</p>}
+            {this.props.wingShape == "SmallTriangle" && <p className="robot__error--wing typewriter b">"The Rocket will not take off with wings that small"</p>}
+            {this.props.thrusterShape == 'Thruster1' && <p className="robot__error--thruster">"Oh no, that's way too small - your're going to have to choose another one"</p>}
             {this.props.windowShape == "twoLargePanels" && <p className="robot__error--window b">"There's no glass in these windows, I'll get sucked into Space!"</p>}
             {this.props.changeColor == "chuckNorris" && <p>"Chuck Norris?! Yeaaaahh Buddy!!</p>}
           </div>
