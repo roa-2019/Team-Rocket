7693db118ccafd30ef7813129476adab06475faf
rosedhlakama <rosejembe@live.com>
2019-10-01 11:26:21 +1300

    robot error message

--- client/components/Rockets.jsx
+++ client/components/Rockets.jsx
@@ -54,7 +54,7 @@ class Rockets extends Component {
             <JvBot />
             {this.props.wingShape == "SmallTriangle" && <p className="robot__error--wing typewriter b">"The Rocket will not take off with wings that small"</p>}
             {this.props.noseShape == "Pyramid" && <p className= "robot__error--nose">"Oops you'll have to choose a nose that isn't wonky"</p>}
-            {this.props.thrusterShape == 'Thruster1' && <p className = "robot__error--thruster">"Oh no, that's way too small - your're going to have to choose another one"</p> }
+            {this.props.thrusterShape == 'Thruster1' && <p className = "robot__error--thruster">"That does not appear to be a thruster. Try another one."</p> }
             {this.props.bodyShape == "Rectangle" && <p className="robot__error--bodyShape">"Do you see me? How am I supposed to fit into such a thin Rocket?!"</p>}
             {this.props.windowShape == "twoLargePanels" && <p className="robot__error--window b">"There's no glass in these windows, I'll get sucked into Space!"</p>} 
             {this.props.changeColor == "chuckNorris" && <p>"Chuck Norris?! Yeaaaahh Buddy!!</p> }
