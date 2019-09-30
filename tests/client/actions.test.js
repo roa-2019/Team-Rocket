d3e9d692e5d6bf92cbc9708a394b7713328ca769
Alex-Abernethy <alexander.abernethy@gmail.com>
2019-10-01 10:31:31 +1300

    removed test suite is working test

--- tests/client/actions.test.js
+++ tests/client/actions.test.js
@@ -1,10 +1,5 @@
 import nock from 'nock'
 import * as actions from '../../client/actions'
-import { getSatellite } from '../../client/apiClient/iss'
-
-test('actions.test suite is working', () => {
-    expect(5 % 2).toEqual(1)
-})
 
 test('noseShape returns a shape', () => {
     let action = actions.noseShape('circle')
