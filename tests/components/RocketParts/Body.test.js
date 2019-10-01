48e2df15d2255facb361502ef35dcef49598dcdc
rosedhlakama <rosejembe@live.com>
2019-10-02 08:21:36 +1300

    Nose compoenent tests complete

--- tests/components/RocketParts/Body.test.js
+++ tests/components/RocketParts/Body.test.js
@@ -26,26 +26,21 @@ jest.mock('react-dom/server', () => {
     }
 })
 
-// test('Body test suite is working', () => {
-//     expect(5 % 2).toEqual(1)
-// })
 
 describe('<Body />', () => {
+    const wrapper = shallow(<Body />)
+    wrapper.setProps({ bodyShape: "Trapezoid", changeColor: "pink", strokeColor: "orange" })
+    let trapezoidPoints = "200 200 380 200 360 350 220 350"
+    let rectanglePoints = "240 200 340 200 340 500 240 500"
+    let polygonColors = "fill=\"pink\" stroke=\"orange\""
+    let otherColors = "fill=\"yellow\" stroke=\"blue\""
     test('Body component props return correct shape', () => {
         expect.assertions(2)
-        const wrapper = shallow(<Body />)
-        wrapper.setProps({bodyShape: 'Trapezoid'})
-        let trapezoidPoints ="200 200 380 200 360 350 220 350"
-        let rectanglePoints = "240 200 340 200 340 500 240 500"
         expect(wrapper.html()).toContain(trapezoidPoints)
         expect(wrapper.html()).not.toContain(rectanglePoints)
     })
     test('Body component color props return correct color', () => {
         expect.assertions(2)
-        const wrapper = shallow(<Body />)
-        wrapper.setProps({bodyShape: "Tower", changeColor: "pink", strokeColor: "orange"})
-        let polygonColors ="fill=\"pink\" stroke=\"orange\""
-        let otherColors = "fill=\"yellow\" stroke=\"blue\""
         expect(wrapper.html()).toContain(polygonColors)
         expect(wrapper.html()).not.toContain(otherColors)
     })
--- tests/components/RocketParts/Nose.test.js
+++ tests/components/RocketParts/Nose.test.js
@@ -2,7 +2,8 @@ import React from 'react'
 import { shallow } from 'enzyme';
 
 
-import Body from '../../../client/components/rocket-parts/Body'
+import Nose from '../../../client/components/rocket-parts/Nose'
+import { exportAllDeclaration } from '@babel/types';
 
 const MockrenderToStringActual = jest.requireActual('react-dom/server').renderToString
 
@@ -24,4 +25,24 @@ jest.mock('react-dom/server', () => {
             return <span>test</span>
         })
     }
+})
+
+
+describe('<Nose />', () => {
+    const wrapper = shallow(<Nose />)
+     wrapper.setProps({noseShape: "Ellipse", changeColor: "Purple", strokeColor:"Red"})
+     let ellispeProperties = "id=\"ellipse\" cx=\"290\" cy=\"210\" rx=\"50\" ry=\"100\""
+     let pyramidProperties = "points=\"700,355 700,0 0,700\""
+     let ellipseColors = " stroke=\"Red\" fill=\"Purple\" "
+     let otherColors = "fill=\"yellow\" stroke=\"blue\""
+    test('Nose component props change return correct shape', () => {
+        expect.assertions(2)
+        expect(wrapper.html()).toContain(ellispeProperties)
+        expect(wrapper.html()).not.toContain(pyramidProperties)
+    })
+    test('Nose compoent color props return correct color', () => {
+        expect.assertions(2)
+        expect(wrapper.html()).toContain(ellipseColors)
+        expect(wrapper.html()).not.toContain(otherColors)
+    })
 })
