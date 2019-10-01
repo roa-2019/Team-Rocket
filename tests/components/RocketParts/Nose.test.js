0f9500dad82477b514a67c47eaac20b97bf7dbec
rosedhlakama <rosejembe@live.com>
2019-10-02 08:51:44 +1300

    refactor nose component tests

--- tests/components/RocketParts/Nose.test.js
+++ tests/components/RocketParts/Nose.test.js
@@ -30,18 +30,11 @@ jest.mock('react-dom/server', () => {
 describe('<Nose />', () => {
     const wrapper = shallow(<Nose />)
      wrapper.setProps({noseShape: "Ellipse", changeColor: "Purple", strokeColor:"Red"})
-     let ellispeProperties = "id=\"ellipse\" cx=\"290\" cy=\"210\" rx=\"50\" ry=\"100\""
-     let pyramidProperties = "points=\"700,355 700,0 0,700\""
-     let ellipseColors = " stroke=\"Red\" fill=\"Purple\" "
-     let otherColors = "fill=\"yellow\" stroke=\"blue\""
+     let ellispeProperties = "ellipse id=\"ellipse\" cx=\"290\" cy=\"210\" rx=\"50\" ry=\"100\" stroke=\"Red\" fill=\"Purple\" stroke-width=\"1\""
+     let otherShape = "points=\"700,355 700,0 0,700\" stroke=\"Red\" fill=\"Purple\""
     test('Nose component props change return correct shape', () => {
         expect.assertions(2)
         expect(wrapper.html()).toContain(ellispeProperties)
-        expect(wrapper.html()).not.toContain(pyramidProperties)
-    })
-    test('Nose compoent color props return correct color', () => {
-        expect.assertions(2)
-        expect(wrapper.html()).toContain(ellipseColors)
-        expect(wrapper.html()).not.toContain(otherColors)
+        expect(wrapper.html()).not.toContain(otherShape)
     })
 })
