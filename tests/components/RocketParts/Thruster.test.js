3e0e4d6ebc3afa00693d121c53aa6dc82c9383e3
rosedhlakama <rosejembe@live.com>
2019-10-02 08:45:44 +1300

    thurster component tests added

--- tests/components/RocketParts/Thruster.test.js
+++ tests/components/RocketParts/Thruster.test.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { shallow } from 'enzyme';
 
 
-import Nose from '../../../client/components/rocket-parts/Nose'
+import Thruster from '../../../client/components/rocket-parts/Thruster'
 
 const MockrenderToStringActual = jest.requireActual('react-dom/server').renderToString
 
@@ -28,4 +28,16 @@ jest.mock('react-dom/server', () => {
 
 test("Thruster test suite is working", () => {
     expect(true).toBeTruthy
+})
+
+describe('<Nose />', () => {
+    const wrapper = shallow(<Thruster />)
+    wrapper.setProps({ thrusterShape: "Thruster3", changeColor: "pink", strokeColor: "grey" })
+    let thrusterThree = "polygon points=\"320 500 300 540 280 540 260 500\" fill=\"pink\" stroke=\"grey\" stroke-width=\"1\""
+    let thrusterTwo = " points=\"320 500 350 540 230 540 260 500\""
+    test('Nose component props change', () => {
+        expect.assertions(2)
+        expect(wrapper.html()).toContain(thrusterThree)
+        expect(wrapper.html()).not.toContain(thrusterTwo)
+    })
 })
