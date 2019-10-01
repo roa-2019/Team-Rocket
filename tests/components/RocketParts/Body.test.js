f8be47797de081d5688830225afb39889cad69cb
rosedhlakama <rosejembe@live.com>
2019-10-02 08:54:29 +1300

    refactor body component tests

--- tests/components/RocketParts/Body.test.js
+++ tests/components/RocketParts/Body.test.js
@@ -30,18 +30,11 @@ jest.mock('react-dom/server', () => {
 describe('<Body />', () => {
     const wrapper = shallow(<Body />)
     wrapper.setProps({ bodyShape: "Trapezoid", changeColor: "pink", strokeColor: "orange" })
-    let trapezoidPoints = "200 200 380 200 360 350 220 350"
+    let trapezoidProperties = "polygon points=\"200 200 380 200 360 350 220 350 \" fill=\"pink\" stroke=\"orange\" stroke-width=\"1\"></polygon><polygon points=\"240 500 340 500 360 350 220 350\" fill=\"pink\" stroke=\"orange\" stroke-width=\"1\""
     let rectanglePoints = "240 200 340 200 340 500 240 500"
-    let polygonColors = "fill=\"pink\" stroke=\"orange\""
-    let otherColors = "fill=\"yellow\" stroke=\"blue\""
     test('Body component props return correct shape', () => {
         expect.assertions(2)
-        expect(wrapper.html()).toContain(trapezoidPoints)
+        expect(wrapper.html()).toContain(trapezoidProperties)
         expect(wrapper.html()).not.toContain(rectanglePoints)
-    })
-    test('Body component color props return correct color', () => {
-        expect.assertions(2)
-        expect(wrapper.html()).toContain(polygonColors)
-        expect(wrapper.html()).not.toContain(otherColors)
     })
 })
